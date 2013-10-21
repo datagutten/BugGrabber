33696f0256a5d9977cc96addf5727794d14bdb42
funkydude <funkydude@0924a26b-0b9e-45b9-823b-2f821e446298>
2013-10-21 13:55:05 +0000

    Add Italian and fixed translations on wowace. Translators note than all strings have been changed and will need checked.

--- trunk/locales.lua
+++ trunk/locales.lua
@@ -12,23 +12,25 @@ if not addon then return end
 
 function addon:LoadTranslations(locale, L)
 	if locale == "koKR" then
---@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "deDE" then
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "esES" then
---@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "zhTW" then
---@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "zhCN" then
---@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "ruRU" then
---@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "frFR" then
---@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "esMX" then
---@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="ignore")@
 	elseif locale == "ptBR" then
---@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=true)@
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="ignore")@
+	elseif locale == "itIT" then
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="ignore")@
 	end
 end
 
